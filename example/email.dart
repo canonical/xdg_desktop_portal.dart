87cbe229eea4e5a4c6648201df28e4cf8a4de58e
Robert Ancell <robert.ancell@canonical.com>
2022-07-29 16:11:28 +1200

    Hide request object, throw exceptions instead

--- example/email.dart
+++ example/email.dart
@@ -2,10 +2,7 @@ import 'package:xdg_desktop_portal/xdg_desktop_portal.dart';
 
 void main(List<String> args) async {
   var client = XdgDesktopPortalClient();
-  var request = await client.email.composeEmail();
-  if (await request.response != XdgPortalResponse.success) {
-    print('Failed to compose email');
-  }
+  await client.email.composeEmail();
 
   await client.close();
 }
