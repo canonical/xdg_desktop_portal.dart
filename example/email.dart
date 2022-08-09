091e0ca5ea54e941bc0b16ede4374782cfae57de
Robert Ancell <robert.ancell@canonical.com>
2022-08-10 09:23:41 +1200

    Add some content in the email example

--- example/email.dart
+++ example/email.dart
@@ -2,7 +2,8 @@ import 'package:xdg_desktop_portal/xdg_desktop_portal.dart';
 
 void main(List<String> args) async {
   var client = XdgDesktopPortalClient();
-  await client.email.composeEmail();
+  await client.email
+      .composeEmail(address: 'dart@example.com', subject: 'Tell me about Dart');
 
   await client.close();
 }
