ea73fd18fe40c111bee15dcbe7775f7426d7ab79
Robert Ancell <robert.ancell@canonical.com>
2022-08-11 11:44:22 +1200

    Move response handler into XdgPortalRequest - this simplifies the client from needing to maintain a list of requests

--- lib/src/xdg_portal_request.dart
+++ lib/src/xdg_portal_request.dart
@@ -20,23 +20,39 @@ class XdgPortalRequest {
   /// Stream containing the single result returned from the portal.
   Stream<Map<String, DBusValue>> get stream => _controller.stream;
 
-  final Future<DBusRemoteObject> Function(XdgPortalRequest) _send;
+  final DBusRemoteObject _portalObject;
+  StreamSubscription? _requestResponseSubscription;
+  final Future<DBusObjectPath> Function() _send;
   late final StreamController<Map<String, DBusValue>> _controller;
   final _listenCompleter = Completer();
   late final DBusRemoteObject _object;
   var _haveResponse = false;
 
-  XdgPortalRequest(this._send) {
+  XdgPortalRequest(this._portalObject, this._send) {
     _controller = StreamController<Map<String, DBusValue>>(
         onListen: _onListen, onCancel: _onCancel);
   }
 
   Future<void> _onListen() async {
-    _object = await _send(this);
+    var requestResponse = DBusSignalStream(_portalObject.client,
+        interface: 'org.freedesktop.portal.Request',
+        name: 'Response',
+        signature: DBusSignature('ua{sv}'));
+    _requestResponseSubscription = requestResponse.listen((signal) {
+      if (signal.path == _object.path) {
+        _handleResponse(signal.values[0].asUint32(),
+            signal.values[1].asStringVariantDict());
+      }
+    });
+
+    _object = DBusRemoteObject(_portalObject.client,
+        name: _portalObject.name, path: await _send());
     _listenCompleter.complete();
   }
 
   Future<void> _onCancel() async {
+    await _requestResponseSubscription?.cancel();
+
     // Ensure that we have started the stream
     await _listenCompleter.future;
 
@@ -51,8 +67,7 @@ class XdgPortalRequest {
     }
   }
 
-  /// Process the response received for this request.
-  void handleResponse(int response, Map<String, DBusValue> result) {
+  void _handleResponse(int response, Map<String, DBusValue> result) {
     _haveResponse = true;
     switch (response) {
       case 0:
