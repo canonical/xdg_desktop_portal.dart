c21c897b7ff2193cc31801f63c172dae782d9cd7
aospiridonov <aospiridonov@gmail.com>
2022-08-16 22:19:31 +0300

    fix work start and selectSpurces

--- example/screencast.dart
+++ example/screencast.dart
@@ -2,17 +2,13 @@ import 'package:xdg_desktop_portal/xdg_desktop_portal.dart';
 
 void main(List<String> args) async {
   var client = XdgDesktopPortalClient();
-  print('createSession');
   await client.screenCast.createSession();
-  print('selectSources');
-  await client.screenCast.selectSources(
-    sourceTypes: {ScreenCastAvailableSourceTypes.monitor},
-    cursorModes: {ScreenCastAvailableCursorModes.hidden},
-  );
-  print('start');
-  await client.screenCast.start();
-  print('openPipeWireRemote');
+  await client.screenCast.selectSources(multiple: true);
+  final screenCastStreams = await client.screenCast.start();
+  for (final stream in screenCastStreams) {
+    print(stream);
+  }
   final fd = await client.screenCast.openPipeWireRemote();
   print(fd);
-  await client.screenCast.close();
+  await client.close();
 }
--- lib/src/xdg_screen_cast_portal.dart
+++ lib/src/xdg_screen_cast_portal.dart
@@ -4,13 +4,12 @@ import 'dart:io';
 import 'package:dbus/dbus.dart';
 
 import 'xdg_portal_request.dart';
-import 'xdg_portal_session.dart';
 
 /// Source types of screen cast.
-enum ScreenCastAvailableSourceTypes { monitor, window, virtual }
+enum ScreenCastAvailableSourceType { monitor, window, virtual }
 
 /// Cursor modes of screen cast.
-enum ScreenCastAvailableCursorModes { hidden, embedded, metadata }
+enum ScreenCastAvailableCursorMode { hidden, embedded, metadata }
 
 /// Persist mode of screen cast session.
 /// Remote desktop screen cast sessions cannot persist.
@@ -26,13 +25,44 @@ enum ScreenCastSessionPersistMode {
   untilRevoked
 }
 
+class ScreenCastStream {
+  final int nodeId;
+  final String id;
+  final List<int> position;
+  final List<int> size;
+  final ScreenCastAvailableSourceType sourceType;
+
+  ScreenCastStream({
+    required this.nodeId,
+    required this.id,
+    required this.position,
+    required this.size,
+    required this.sourceType,
+  });
+  @override
+  int get hashCode => Object.hash(
+      nodeId, id, Object.hashAll(position), Object.hashAll(size), sourceType);
+
+  @override
+  bool operator ==(other) =>
+      other is ScreenCastStream &&
+      other.nodeId == nodeId &&
+      other.id == id &&
+      other.position == position &&
+      other.size == size &&
+      other.sourceType == sourceType;
+
+  @override
+  String toString() =>
+      '$runtimeType(node ID: $nodeId, id: $id, position: $position, size: $size, sourceType: $sourceType})';
+}
+
 /// Screen cast portal.
 class XdgScreenCastPortal {
   final DBusRemoteObject _object;
   final String Function() _generateToken;
 
-  XdgPortalSession? _session;
-  StreamSubscription? _sessionSubscription;
+  DBusObjectPath? _sessionPath;
 
   XdgScreenCastPortal(this._object, this._generateToken);
 
@@ -43,39 +73,39 @@ class XdgScreenCastPortal {
       .then((v) => v.asUint32());
 
   /// Get the available screen cast source types.
-  Future<Set<ScreenCastAvailableSourceTypes>> getAvailableSourceTypes() =>
+  Future<Set<ScreenCastAvailableSourceType>> getAvailableSourceTypes() =>
       _object
           .getProperty(
               'org.freedesktop.portal.ScreenCast', 'AvailableSourceTypes',
               signature: DBusSignature('u'))
           .then((v) {
         final value = v.asUint32();
-        final types = <ScreenCastAvailableSourceTypes>{};
-        if (value & 1 != 0) types.add(ScreenCastAvailableSourceTypes.monitor);
-        if (value & 2 != 0) types.add(ScreenCastAvailableSourceTypes.window);
-        if (value & 4 != 0) types.add(ScreenCastAvailableSourceTypes.virtual);
+        final types = <ScreenCastAvailableSourceType>{};
+        if (value & 1 != 0) types.add(ScreenCastAvailableSourceType.monitor);
+        if (value & 2 != 0) types.add(ScreenCastAvailableSourceType.window);
+        if (value & 4 != 0) types.add(ScreenCastAvailableSourceType.virtual);
         return types;
       });
 
   /// Get the available screen cast cursor modes.
-  Future<Set<ScreenCastAvailableCursorModes>> getAvailableCursorModes() =>
+  Future<Set<ScreenCastAvailableCursorMode>> getAvailableCursorModes() =>
       _object
           .getProperty(
               'org.freedesktop.portal.ScreenCast', 'AvailableCursorModes',
               signature: DBusSignature('u'))
           .then((v) {
         final value = v.asUint32();
-        final types = <ScreenCastAvailableCursorModes>{};
-        if (value & 1 != 0) types.add(ScreenCastAvailableCursorModes.hidden);
-        if (value & 2 != 0) types.add(ScreenCastAvailableCursorModes.embedded);
-        if (value & 4 != 0) types.add(ScreenCastAvailableCursorModes.metadata);
+        final types = <ScreenCastAvailableCursorMode>{};
+        if (value & 1 != 0) types.add(ScreenCastAvailableCursorMode.hidden);
+        if (value & 2 != 0) types.add(ScreenCastAvailableCursorMode.embedded);
+        if (value & 4 != 0) types.add(ScreenCastAvailableCursorMode.metadata);
         return types;
       });
 
   /// Create a location session that returns a stream of location updates from the portal.
   /// When the session is no longer required close the stream.
   Future<void> createSession() async {
-    _session = XdgPortalSession(_object, () async {
+    var request = XdgPortalRequest(_object, () async {
       var options = <String, DBusValue>{};
       options['handle_token'] = DBusString(_generateToken());
       options['session_handle_token'] = DBusString(_generateToken());
@@ -86,23 +116,18 @@ class XdgScreenCastPortal {
           replySignature: DBusSignature('o'));
       return createResult.returnValues[0].asObjectPath();
     });
-    _sessionSubscription = _session!.stream.listen((_) {
-      print('onData');
-    }, onDone: () async {
-      print('onDone');
-    });
-    bool value = await _session!.created;
-    print(value);
-    //final value2 = _session!.stream.first;
-    //print(value2);
+
+    final result = await request.stream.first;
+    final sessionHandle = result['session_handle']?.asString() ?? '';
+    _sessionPath = DBusObjectPath(sessionHandle);
   }
 
   /// Configure what the screen cast session should record.
   /// This method must be called before starting the session.
   Future<void> selectSources({
-    Set<ScreenCastAvailableSourceTypes>? sourceTypes,
+    Set<ScreenCastAvailableSourceType>? sourceTypes,
     bool? multiple,
-    Set<ScreenCastAvailableCursorModes>? cursorModes,
+    Set<ScreenCastAvailableCursorMode>? cursorModes,
     String? restoreToken,
     ScreenCastSessionPersistMode? persistMode,
   }) async {
@@ -112,9 +137,9 @@ class XdgScreenCastPortal {
       if (sourceTypes != null) {
         int bitmask = 0;
         for (final type in sourceTypes) {
-          if (type == ScreenCastAvailableSourceTypes.monitor) bitmask |= 1;
-          if (type == ScreenCastAvailableSourceTypes.window) bitmask |= 2;
-          if (type == ScreenCastAvailableSourceTypes.virtual) bitmask |= 4;
+          if (type == ScreenCastAvailableSourceType.monitor) bitmask |= 1;
+          if (type == ScreenCastAvailableSourceType.window) bitmask |= 2;
+          if (type == ScreenCastAvailableSourceType.virtual) bitmask |= 4;
         }
         options['types'] = DBusUint32(bitmask);
       }
@@ -124,9 +149,9 @@ class XdgScreenCastPortal {
       if (cursorModes != null) {
         int bitmask = 0;
         for (final mode in cursorModes) {
-          if (mode == ScreenCastAvailableCursorModes.hidden) bitmask |= 1;
-          if (mode == ScreenCastAvailableCursorModes.embedded) bitmask |= 2;
-          if (mode == ScreenCastAvailableCursorModes.metadata) bitmask |= 4;
+          if (mode == ScreenCastAvailableCursorMode.hidden) bitmask |= 1;
+          if (mode == ScreenCastAvailableCursorMode.embedded) bitmask |= 2;
+          if (mode == ScreenCastAvailableCursorMode.metadata) bitmask |= 4;
         }
         options['cursor_mode'] = DBusUint32(bitmask);
       }
@@ -136,34 +161,11 @@ class XdgScreenCastPortal {
       if (persistMode != null) {
         options['persist_mode'] = DBusUint32(persistMode.index);
       }
-      print('session: ${_session!.object!.path}');
-      print('options: $options');
       var result = await _object.callMethod(
         'org.freedesktop.portal.ScreenCast',
         'SelectSources',
         [
-          _session!.object!.path,
-          DBusDict.stringVariant(options),
-        ],
-        replySignature: DBusSignature('o'),
-      );
-      return result.returnValues[0].asObjectPath();
-    });
-    final result = await request.stream.first;
-    print(result);
-  }
-
-  Future<void> start({String parentWindow = ''}) async {
-    var request = XdgPortalRequest(_object, () async {
-      var options = <String, DBusValue>{};
-      options['handle_token'] = DBusString(_generateToken());
-
-      var result = await _object.callMethod(
-        'org.freedesktop.portal.ScreenCast',
-        'Start',
-        [
-          _session!.object!.path,
-          DBusString(parentWindow),
+          _sessionPath!,
           DBusDict.stringVariant(options),
         ],
         replySignature: DBusSignature('o'),
@@ -173,6 +175,53 @@ class XdgScreenCastPortal {
     await request.stream.first;
   }
 
+  Future<List<ScreenCastStream>> start({String parentWindow = ''}) async {
+    var request = XdgPortalRequest(_object, () async {
+      var options = <String, DBusValue>{};
+      options['handle_token'] = DBusString(_generateToken());
+      var result = await _object.callMethod(
+        'org.freedesktop.portal.ScreenCast',
+        'Start',
+        [
+          _sessionPath!,
+          DBusString(parentWindow),
+          DBusDict.stringVariant(options),
+        ],
+        replySignature: DBusSignature('o'),
+      );
+      return result.returnValues[0].asObjectPath();
+    });
+    final resultResult = await request.stream.first;
+    final streams = resultResult['streams']!.asArray();
+    return streams.map((stream) {
+      final params = stream.asStruct();
+      final nodeId = params[0].asUint32();
+      final properties = params[1].asStringVariantDict();
+      final id = properties['id']!.asString();
+      final sourceTypeValue = properties['source_type']!.asUint32();
+      ScreenCastAvailableSourceType sourceType =
+          ScreenCastAvailableSourceType.monitor;
+      if (sourceTypeValue & 1 != 0) {
+        sourceType = ScreenCastAvailableSourceType.monitor;
+      } else if (sourceTypeValue & 2 != 0) {
+        sourceType = ScreenCastAvailableSourceType.window;
+      } else if (sourceTypeValue & 4 != 0) {
+        sourceType = ScreenCastAvailableSourceType.virtual;
+      }
+      final positionArray = properties['position']!.asStruct();
+      final position = [positionArray[0].asInt32(), positionArray[1].asInt32()];
+      final sizeArray = properties['size']!.asStruct();
+      final size = [sizeArray[0].asInt32(), sizeArray[1].asInt32()];
+      return ScreenCastStream(
+        nodeId: nodeId,
+        id: id,
+        sourceType: sourceType,
+        position: position,
+        size: size,
+      );
+    }).toList();
+  }
+
   /// Open a file descriptor to the PipeWire remote where the camera nodes are available.
   Future<ResourceHandle> openPipeWireRemote() async {
     var options = <String, DBusValue>{};
@@ -180,15 +229,11 @@ class XdgScreenCastPortal {
       'org.freedesktop.portal.ScreenCast',
       'OpenPipeWireRemote',
       [
-        _session!.object!.path,
+        _sessionPath!,
         DBusDict.stringVariant(options),
       ],
       replySignature: DBusSignature('h'),
     );
     return result.returnValues[0].asUnixFd();
-  }
-
-  Future<void> close() async {
-    await _sessionSubscription?.cancel();
   }
 }
