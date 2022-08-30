4b6cfd145c3d7267355623f166c6d8da89c3591a
Robert Ancell <robert.ancell@canonical.com>
2022-08-30 16:59:05 +1200

    Add XdgSettingsPortal.settingChanged stream
    Fixes https://github.com/canonical/xdg_desktop_portal.dart/issues/81

--- example/settings.dart
+++ example/settings.dart
@@ -1,22 +1,41 @@
 import 'package:xdg_desktop_portal/xdg_desktop_portal.dart';
 
+void usage() {
+  print('Usage:');
+  print('  settings get <namespace> [<key>]');
+  print('  settings monitor');
+}
+
 void main(List<String> args) async {
   if (args.isEmpty) {
-    print('Usage settings <namespace> [<key>]');
+    usage();
     return;
   }
-  var namespace = args[0];
+  var command = args[0];
 
   var client = XdgDesktopPortalClient();
-  if (args.length > 1) {
-    var key = args[1];
-    var value = await client.settings.read(namespace, key);
-    print('${value.toNative()}');
-  } else {
-    var values = await client.settings.readAll([namespace]);
-    for (var entry in (values[namespace] ?? {}).entries) {
-      print('${entry.key}: ${entry.value.toNative()}');
-    }
+  switch (command) {
+    case 'get':
+      var namespace = args[1];
+      if (args.length > 2) {
+        var key = args[2];
+        var value = await client.settings.read(namespace, key);
+        print('${value.toNative()}');
+      } else {
+        var values = await client.settings.readAll([namespace]);
+        for (var entry in (values[namespace] ?? {}).entries) {
+          print('${entry.key}: ${entry.value.toNative()}');
+        }
+      }
+      break;
+    case 'monitor':
+      await for (var value in client.settings.settingChanged) {
+        print('${value.namespace}.${value.key} = ${value.value.toNative()}');
+      }
+      break;
+    default:
+      usage();
+      break;
   }
 
   await client.close();
