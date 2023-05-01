94f3c6f8c3de1ead60f700babbbf840249606c50
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-05-01 14:23:27 -0400

    feat(notification): add ActionInvoked signal
    Resolves #31

--- example/notification.dart
+++ example/notification.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:xdg_desktop_portal/xdg_desktop_portal.dart';
 
 void usage() {
   print('Usage:');
   print('  notification add <id> <title> <body>');
   print('  notification remove <id>');
+  print(
+    '  notification monitor <id> <defaultAction> <button1Label> <button1Action> <button2Label> <button2Action>',
+  );
 }
 
 void main(List<String> args) async {
@@ -27,6 +32,35 @@ void main(List<String> args) async {
       break;
     case 'remove':
       await client.notification.removeNotification(id);
+      break;
+    case 'monitor':
+      if (args.length != 7) {
+        usage();
+        break;
+      }
+      client.notification.actionInvoked.listen((event) {
+        print(event);
+        exit(0);
+      });
+      var title = 'Example title';
+      var body = 'Example body';
+      await client.notification.addNotification(
+        id,
+        title: title,
+        body: body,
+        defaultAction: args[2],
+        buttons: [
+          XdgNotificationButton(
+            label: args[3],
+            action: args[4],
+          ),
+          XdgNotificationButton(
+            label: args[5],
+            action: args[6],
+          ),
+        ],
+      );
+      await Future.delayed(Duration(seconds: 5));
       break;
     default:
       usage();
